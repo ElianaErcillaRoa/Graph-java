186c538319ec0530e86cfe00559c84566db51f40
ELIANA <eerciliar@est.emi.edu.bo>
2020-10-15 18:30:42 -0400

    IMPLEMENTACION DE LA FUNCION printGraph

---
+++ data/structure/State.java
@@ -0,0 +1,11 @@
+package data.structure;
+
+public class State {
+
+	public static final State NO_VISITADO = null;
+	public static final State PROCESADO = null;
+	public static final State VISITADO = null;
+
+	
+
+}
